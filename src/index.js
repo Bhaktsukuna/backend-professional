f1d860599507ee2e0f5b09cfacce97aa801659ed
Yuva Kumar <yuvakumar439@gmail.com>
2026-01-06 15:50:51 +0530

    utills code discussion

--- src/index.js
+++ src/index.js
@@ -1,37 +1,25 @@
 // require("dotenv").config({path: "./env"})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
-  path: "./env"
-})
+  path: "./env",
+});
 
-
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    const server = app.listen(process.env.PORT, () => {
+      console.log(`Server is running at PORT ${process.env.PORT}`);
+    });
+    server.on("error",(err)=>{
+         console.error("SERVER ERROR:", err);
+    })
+  })
+  .catch((err) => {
+    console.log("MONGO DB connectrion Failed", err);
+      process.exit(1);
+  });
 
 /*
 import express from "express"
@@ -41,14 +29,15 @@ const app = express()
 ;(async()=>{
 try {
 await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-app.on("error",(error)=>{
-  console.error("ERROR:", error);
-  throw error
-})
 
-app.listen(process.env.PORT,()=>{
+
+const server = app.listen(process.env.PORT,()=>{
   console.log(`App is listening on port ${process.env.PORT}`);
 
+})
+
+server.on("error",(error)=>{
+  console.error("ERROR:", error);
 })
 } catch (error) {
 console.error("ERROR:",error);
